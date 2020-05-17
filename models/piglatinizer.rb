a9f0d5f0a9d40aae08cf7a4fb6c4c34cebd38800
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-17 16:40:53 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,11 +3,11 @@ class PigLatinizer
 
 def piglatinize(word) 
   if word.downcase.index(/[aeiou]/) == 0
-    word + "way"
+    return word + "way"
   else
     vowel_index = word.index(/[aeiou]/)
     front_end = word.slice!(0..vowel_index-1)
-    word + front_end +"ay"
+    return word + front_end +"ay"
   end
 end
 
