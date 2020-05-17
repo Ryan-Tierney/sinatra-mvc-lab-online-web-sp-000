56f5e443e94165c3513ee1111719cade276e8ae9
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-17 16:01:18 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,30 +1,18 @@
 class PigLatinizer
-    attr_accessor :piglatinized_prhase
-   def piglatinize(str)
-     vowels = ["a", "e", "i", "o", "u"]
-     two_letter_consonants = ["ch", "sh", "qu", "th", "br", "pr"]
-     three_letter_consonants = ["thr", "sch", "squ"]
-     words = str.split(" ")
-     result = [];
-     words.each do |word|
-      if vowels.include? word[0]
-            result.push word << 'way'
-      else
-          if three_letter_consonants.include? word[0] + word[1] + word[2]
-                first_three_letters = word.slice!(0,3)
-                result.push word << first_three_letters << 'ay'
-          elsif  two_letter_consonants.include? word[0] + word[1]
-                first_two_letters = word.slice!(0,2)
-                result.push word << first_two_letters << 'ay'
-          else
-                first_letter = word.slice!(0)
-                result.push word << first_letter << 'ay'
-                @piglatinized_phrase = result.join(" ")
-          end 
-       end 
-    end 
-        return @piglatinized_phrase
-end 
+  attr_accessor :piglatinized_phrase
 
+def piglatinize(word) #pig latinizes a single word
+  if word.downcase.index(/[aeiou]/) == 0
+    word + "way"
+  else
+    vowel_index = word.index(/[aeiou]/)
+    front_end = word.slice!(0..vowel_index-1)
+    word + front_end +"ay"
+  end
+end
 
+def to_pig_latin(phrase)
+  word_array = phrase.split(" ")
+  @piglatinized_phrase = word_array.collect {|word| piglatinize(word)}.join(" ")
+end
 end 
